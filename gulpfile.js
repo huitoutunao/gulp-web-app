b1967c27895852e50b144aa9b75bf11c2b99b10d
huitoutunao <lh_huitoutunao@163.com>
2022-04-10 20:38:24 +0800

    feat: 新增sass模块导入和第三方css库引用

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ const { src, dest } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const autoprefixer = require('gulp-autoprefixer')
 const sourcemaps = require('gulp-sourcemaps')
+const concat = require('gulp-concat')
 
 const Path = {
   dev: {
-    views: 'src/views/**/*',
+    views: 'src/views/**/*.html',
     script: 'src/script/**/*.js',
     style: 'src/assets/style/',
+    styleLib: 'src/assets/style/lib/*.css',
     images: 'src/assets/images/',
     icons: 'src/assets/icons/',
     font: 'src/assets/font/',
@@ -21,8 +23,15 @@ const Path = {
   },
 }
 
+const cssHandler = function() {
+  return src(Path.dev.styleLib)
+    .pipe(concat('vendors.css'))
+    .pipe(dest(Path.build.style))
+}
+exports.cssHandler = cssHandler
+
 const sassHandler = function() {
-  return src(`${Path.dev.style}*.scss`)
+  return src(`${Path.dev.style}index.scss`)
     .pipe(sourcemaps.init())
     .pipe(autoprefixer())
     .pipe(sass({
